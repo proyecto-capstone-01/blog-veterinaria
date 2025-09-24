89c0864b815c59bd3e53d8c7f8a80673bfb78174
Sergio Neira <s.neira@duocuc.cl>
2025-09-24 00:01:56 -0300

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,25 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Blog Educativo Pucará'
+export const SITE_DESCRIPTION =
+	'A simple theme for personal blog sites, created for Astro framework'
+
+// If you deploy to a subdirectory, set the `WEBSITE_BASE` here.
+// e.g. '/blog' if you are deploying to example.com/blog
+// Leave it as an empty string if you are deploying to a root domain (e.g. example.com)
+export const SITE_BASE = '/saral-theme-astro'
+
+export const SocialLinks = [
+	{
+		name: 'Whatsapp',
+		url: 'https://instagram.com/imyashjawale',
+	},
+]
+
+export const WebsiteLinks = [
+	{
+		name: 'About',
+		url: 'about',
+	},
+]
